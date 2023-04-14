53c9dcc7139724d95f0df6fbf0548a52296f6a1e
Mateus <mateusgoads@gmail.com>
2023-04-14 15:22:25 -0300

    comando sistema de template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    
+    lista = 'Django', 'Python', 'Git', 'HTML', 'Banco de dados', 'Linux', 'Ngiux', 'Uwsgi', 'Systemctl'
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
